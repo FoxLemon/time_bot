0b061558753ecd1d2c6518ffca2b5706a14fd3d0
FoxLemon <75549117+FoxLemon@users.noreply.github.com>
2024-05-03 11:54:52 -0300

    new user set up

--- balance_functions.py
+++ balance_functions.py
@@ -2,13 +2,22 @@
 import json
 
 # Varable
-balance = "user_balances"
+data = r"" #path to your data file
 
 # Load user_balance and return an dict
-def user_balance() -> dict:
-    with open(balance,"r") as file:
-        return json.loads(file)
-# save the new balance by over writing the old balance
-def save_balance(new_balance: dict) -> None:
-    with open(balance,"w") as old_balance:
-        json.dump(new_balance,old_balance,indent=2)
+def user_data() -> dict:
+    file: str = open(data,"r").read()
+    return json.loads(file)
+# Save the new balance by over writing the old balance
+def save_data(new_data: dict) -> None:
+    old_data = open(data,"w")
+    json.dump(new_data,old_data,indent=4)
+# Set up a new data in the json file for new user
+def new_user_data(uid: str,user_name: str) -> None:
+    new_user = user_data()
+    new_user["users"][uid] = dict()
+    new_user["users"][uid]["name"] = user_name
+    new_user["users"][uid]["balance"] = 0
+    new_user["users"][uid]["green_token_balance"] = 0
+    new_user["users"][uid]["message_multiplier"] = 1
+    save_data(new_user)
