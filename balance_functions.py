a9ead2868b05b38aa48a2098ad7c6a396d7c4760
FoxLemon <75549117+FoxLemon@users.noreply.github.com>
2024-05-03 10:02:38 -0300

    save balance function

--- balance_functions.py
+++ balance_functions.py
@@ -8,4 +8,7 @@ balance = "user_balances"
 def user_balance() -> dict:
     with open(balance,"r") as file:
         return json.loads(file)
-# 
+# save the new balance by over writing the old balance
+def save_balance(new_balance: dict) -> None:
+    with open(balance,"w") as old_balance:
+        json.dump(new_balance,old_balance,indent=2)
