c4151574416e2af1ff13e4dcc6324b0a8824e6eb
FoxLemon <75549117+FoxLemon@users.noreply.github.com>
2024-05-13 20:13:54 -0300

    same change forgot to push

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ bot = commands.Bot(command_prefix="!",intents=intents)
 # Handling bot start up
 @bot.event
 async def on_ready() -> None:
-    await bot.tree.sync()
+    for cmd_file in setting.CMDS_DIR.glob("*.py"):
+        if cmd_file.name != "__init__.py":
+            await bot.load_extension(f"slashCmds.{cmd_file.name[:-3]}")
+    bot.tree.copy_global_to(guild=setting.GUILD_ID)
+    await bot.tree.sync(guild=setting.GUILD_ID)
     print(f"Logged in as {bot.user.name} succesfully!")
 
 # On message events: mining etc...
@@ -29,52 +33,6 @@ async def on_message(ctx: Message) -> None:
     
     # command running
     await bot.process_commands(ctx)
-
-# check the balance of the one who call the command
-@bot.command()
-async def balance(ctx: Message) -> None:
-    # Description
-    """Checks your balance"""
-    # Function variable
-    check_user(ctx)
-    data: dict = user_data()
-    uid: str = str(ctx.author.id)
-    #send message
-    await ctx.send(f"{data[uid]["name"]} have {data[uid]["balance"]} in balance, and {data[uid]["green_token_balance"]} green tokens")
-
-# check the balance of the user user wanted
-@bot.command()
-async def balanceof(ctx: Message,user: Member) -> None:
-    # Description
-    """Checks the balance of another user"""
-    # Function variable
-    check_user(ctx)
-    data: dict = user_data()
-    uid: str = str(user.id)
-    #send message
-    await ctx.send(f"{data[uid]["name"]} have {data[uid]["balance"]} in balance, and {data[uid]["green_token_balance"]} green tokens")
-
-# pay the other user
-@bot.command()
-async def pay(ctx: Message, user: Member, amount: int) -> None:
-    # Description
-    """Pay the other user"""
-    # Function variable
-    data: dict = user_data()
-    # balance out from:
-    bOut: dict = data[str(ctx.author.id)]
-    # balance in to:
-    bIn: dict = data[str(user.id)]
-    # transaction
-    bOut["balance"] -= amount
-    bIn["balance"] += amount
-    save_data(data)
-    #send message to the payer
-    await ctx.send(f"you've payed {user.name} ${amount}")
-    #send message to the receiver
-    await ctx.send(f"@{user.name.capitalize()} you have received ${amount} from {ctx.author} ")
-
-    
 
 # Main entery point
 def main() -> None:
